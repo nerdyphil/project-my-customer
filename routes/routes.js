582f6fab6f9b3a1671c414784db11358cceecaa4
nerdyphil <owoeyepo@gmail.com>
2020-06-18 13:46:49 +0100

    new file:   controllers/transaction.controller.js 	new file:   routes/routes.js

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+    const transactions = require('../controllers/transaction.controller.js');
+
+    // Create a new Transaction
+    app.post('/transactions', transactions.create);
+
+    // Retrieve all Transactions
+    app.get('/transactions', transactions.findAll);
+
+    // Retrieve a single Transaction with transaction_id
+    app.get('/transactions/:transaction_id', transactions.findOne);
+
+    // Update a Transaction with transaction_id
+    app.put('/transactions/:transaction_id', transactions.update);
+
+    // Delete a Transaction with transaction_id
+    app.delete('/transactions/:transaction_id', transactions.delete);
+}
